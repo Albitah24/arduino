533ad2ec965c8fd70e56db499539613b916d833d
Albitah24 <78345742+Albitah24@users.noreply.github.com>
2021-02-08 10:11:10 +0100

    Update and rename sniper_kill_switch.cpp to snipper_kill_switch.cpp

--- sniper_kill_switch.cpp
+++
@@ -1,8 +0,0 @@
-// colocar en la sección de variables globales
-
-int switchState=0;
-bool isTheButtonBeingPressed= false;
-bool pimPimFlag = false;
-
-//recordar que el pin del botón puede cambiar
-const int buttonPin=5;
