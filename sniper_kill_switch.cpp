ed6b9b94506113f4a8d7b158d2f7346afb3d68b7
Albitah24 <78345742+Albitah24@users.noreply.github.com>
2021-02-08 09:47:27 +0100

    Create sniper_kill_switch.cpp

---
+++ sniper_kill_switch.cpp
@@ -0,0 +1,8 @@
+// colocar en la sección de variables globales
+
+int switchState=0;
+bool isTheButtonBeingPressed= false;
+bool pimPimFlag = false;
+
+//recordar que el pin del botón puede cambiar
+const int buttonPin=5;
