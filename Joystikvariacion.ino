e99eced6c08a1eec2820e28b2758a17744bbde24
Albitah24 <78345742+Albitah24@users.noreply.github.com>
2021-03-02 10:38:40 +0100

    Add files via upload

---
+++ Joystikvariacion.ino
@@ -0,0 +1,33 @@
+//*  Documentación del programa 
+//*  Autore: Alba
+//*  Fecha: 2/3/2021
+//*  Código referencia: (url)
+//*  Descripción del programa:
+// Hardware necesario:
+
+
+//Definiciones de constantes de pines
+const int pinBoton = 3; 
+const int pinEjeY = A1; 
+const int pinEjeX = A0; 
+const int pinLed = 9;
+
+ int estadoBoton = 1;
+
+void setup() {
+//inicializaciones
+  pinMode(pinBoton, INPUT);
+  digitalWrite(pinBoton, HIGH);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  estadoBoton = digitalRead(pinBoton);
+  if (estadoBoton == LOW){
+    digitalWrite(pinLed, HIGH);
+  }
+else{
+  digitalWrite(pinLed, LOW);
+}
+}
