a4bac5a8f7b6daa6f14bcdd58399654a44e1978a
Albitah24 <78345742+Albitah24@users.noreply.github.com>
2021-03-09 08:55:18 +0100

    Add files via upload

--- bolade_cristal.ino
+++ bolade_cristal.ino
@@ -1,6 +1,6 @@
-#inlude <LiquidCeystal.h>
-LiquidCeystal lcd(12, 11, 5, 4, 3, 2);
-const int lcd(switchPin = 6;);
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int switchPin = 6;
 int switchState = 0;
 int prevSwitchState= 0;
 int reply;
@@ -14,6 +14,40 @@ lcd.print ("la bola");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ switchState = digitalRead(switchPin);
+ if (switchState != prevSwitchState){
+  if (switchState == LOW){
+    reply = random(8);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("La bola dice:");
+    switch(reply){
+      case 0:
+      lcd.print("SI");
+      break;
+      case 1:
+      lcd.print("Puede se");
+      break;
+      case 2:
+      lcd.print("Aceptamos pulpo como animal de compañia");
+      break;
+      case 3:
+      lcd.print("Tiniene buena pinta");
+      break;
+      case 4:
+      lcd.print("No puedo, tengo furbo");
+      break;
+      case 5:
+       lcd.print("Kas dixo?");
+      break;
+      case 6:
+       lcd.print("TENGO MIEDO!");
+      break;
+      case 7:
+       lcd.print("NO");
+      break;
+    }
+  }
+ }
+ prevSwitchState = switchState;
 }
