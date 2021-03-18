6b19b0aa41433ed9caccd0a2f9ab10bd64f8101f
Albitah24 <78345742+Albitah24@users.noreply.github.com>
2021-03-18 13:23:13 +0100

    Add files via upload

---
+++ led_apaga_y_enciende_con_dos_botones__18-03-2021.ino
@@ -0,0 +1,32 @@
+
+const int botonPin = 7;
+const int botonPin2 = 5;
+const int ledpin2 = 9;
+int estadoBoton1 =0;
+int estadoBoton2 =0;
+
+void setup() {
+pinMode(ledpin2, OUTPUT);
+pinMode(botonPin, INPUT);
+pinMode(botonPin2, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  
+estadoBoton1 = digitalRead(botonPin);
+estadoBoton2 = digitalRead(botonPin2);
+
+if (estadoBoton1 == HIGH) {
+digitalWrite(ledpin2, HIGH);
+
+}
+if (estadoBoton2 == HIGH) {
+digitalWrite(ledpin2, LOW);
+
+}
+
+
+
+
+}
